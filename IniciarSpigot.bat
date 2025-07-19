d0aef74d02ec7e5c1cfb4ece634d8f01b02db075
Víctor Mejía <vmejiaec@gmail.com>
2025-07-18 22:37:54 -0500

    Configuración de Paper Servidor

--- IniciarSpigot.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Iniciando el servidor de Spigot...
-docker-compose -f .\Spigot\docker-compose.yml up servidor
-
-echo.
-echo Servidor Spigot iniciado. Para detenerlo usa DetenerSpigot.bat.
---
+++ Paper/servidor/iniciarPaper.bat
@@ -0,0 +1,5 @@
+@echo off
+title Servidor Paper - NPC Plugin
+cd /d "%~dp0"
+java -Xmx2G -Xms1G -jar paper-1.21.6-48.jar --nogui
+pause
